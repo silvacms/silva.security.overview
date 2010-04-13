077b93fe4f281e14ba19feec280ccb0dc73d862a
Antonin Amand <antonin.amand@gmail.com>
2010-04-13 14:12:08 +0000

    implement building of the user/role index

--- src/silva/security/overview/service.py
+++ src/silva/security/overview/service.py
@@ -8,6 +8,7 @@ from zope.catalog.interfaces import ICatalog
 from zope.lifecycleevent.interfaces import IObjectCreatedEvent
 
 from silva.core.services.base import SilvaService
+from silva.core.services.utils import walk_silva_tree
 from silva.core import conf as silvaconf
 
 from silva.security.overview.interfaces import ISecurityOverviewService
@@ -16,10 +17,9 @@ from silva.core.interfaces import (ISecurityRoleAddedEvent,
 from silva.security.overview.interfaces import IUserList
 from silva.core.views import views as silvaviews
 
-def build_index():
-    catalog = Catalog()
-    catalog['usernames'] = KeywordIndex('usernames', IUserList, True)
-    return catalog
+
+from logging import getLogger
+logger = getLogger('silva.security.overview.service')
 
 
 class UserList(grok.Adapter):
@@ -39,40 +39,70 @@ class SecurityOverviewService(SilvaService):
     silvaconf.icon('service.png')
 
     manage_options = (
-        {'label':'Security overview', 'action':'manage_overview'},
+        {'label':'Security overview', 'action':'manage_main'},
+        {'label':'Configuration', 'action': 'manage_config'},
         ) + SilvaService.manage_options
 
     def __init__(self, id, title):
         super(SecurityOverviewService, self).__init__
-        self.catalog = build_index()
+        self.catalog = self._build_catalog()
 
     def cleanup(self):
         """ Remove the entire catalog and recreates it
         """
         del self.catalog
-        self.catalog = build_index()
+        self.catalog = self._build_catalog()
 
-    def build(self):
+    def build(self, root=None):
         self.cleanup()
+        root = root or self.get_root()
+        intids = getUtility(IIntIds)
+        count = 0
+        for content in enumerate(walk_silva_tree(root)):
+            if self.index_object(content, intids): count += 1
+        return count
+
+    def index_object(self, ob, intutil=None):
+        intids = intutil or getUtility(IIntIds)
+        try:
+            id = intids.register(ob)
+            self.catalog.index_doc(id, ob)
+            return ob
+        except KeyError:
+            return None
+
+    def _build_catalog(self):
+        catalog = Catalog()
+        catalog['usernames'] = KeywordIndex('usernames', IUserList, True)
+        return catalog
 
 
-
-@grok.subscribe(ISilvaObject, ISecurityRoleAddedEvent)
-def RoleAdded(ob, event):
-    if NoAutoIndex.providedBy(ob): return
-    service = getUtility(ISecurityOverviewService)
-    intids = getUtility(IIntIds)
-    id = intids.getId(ob)
-    service.catalog.index_doc(id, ob)
+@grok.subscribe(ISecurityRoleAddedEvent)
+def RoleAdded(event):
+    try:
+        ob = event.object
+        logger.debug("event role add on %s" % "/".join(ob.getPhysicalPath()))
+        if INoAutoIndex.providedBy(ob): return
+        service = getUtility(ISecurityOverviewService)
+        service.index_object(ob)
+    except (Exception,), e:
+        logger.error('error in ISecurityRoleAddedEvent subscriber for'
+            ' silva.security.overview : %s' % str(e))
+        raise
 
 
 @grok.subscribe(ISecurityRoleRemovedEvent)
-def RoleRemoved(ob, event):
-    if NoAutoIndex.providedBy(ob): return
-    service = getUtility(ISecurityOverviewService)
-    intids = getUtility(IIntIds)
-    id = intids.getId(ob)
-    service.catalog.index_doc(id, ob)
+def RoleRemoved(event):
+    try:
+        ob = event.object
+        logger.debug("event role remove on %s" % "/".join(ob.getPhysicalPath()))
+        if NoAutoIndex.providedBy(ob): return
+        service = getUtility(ISecurityOverviewService)
+        service.index_object(ob)
+    except (Exception,), e:
+        logger.error('error in ISecurityRoleRemovedEvent subscriber for'
+            ' silva.security.overview : %s' % str(e))
+        raise
 
 
 @grok.subscribe(ISecurityOverviewService, IObjectCreatedEvent)
@@ -86,9 +116,23 @@ def configureReferenceService(service, event):
 
 
 class SecurityOverView(silvaviews.ZMIView):
-    grok.name('manage_overview')
+    grok.name('manage_main')
 
     def update(self):
-        pass
+        catalog = self.context.catalog
+        catalog.apply({'usernames': 'admin'})
+        self.entries = catalog.searchResults() or []
+
+
+class SecurityConfig(silvaviews.ZMIView):
+    grok.name('manage_config')
+
+    message = ''
+
+    def update(self):
+        if self.request.method.lower() == 'post':
+            if self.request.get('rebuild'):
+                count = self.context.build()
+                self.message = '%d objects indexed.' % count
 
 
