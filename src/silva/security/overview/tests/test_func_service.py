31f4f404fbb7fbdaf714851a904f43b4f56a8b7f
Sylvain Viollon <thefunny@gmail.com>
2010-09-28 10:36:49 +0000

    Fix functional tests. You need to be manager to use the service.

--- src/silva/security/overview/tests/test_func_service.py
+++ src/silva/security/overview/tests/test_func_service.py
@@ -1,15 +1,17 @@
-from infrae.testbrowser.browser import Browser
-from silva.security.overview.tests.test_service import (add_roles,
-    TestBase)
+
+from silva.security.overview.tests.test_service import (
+    add_roles, TestBase)
 import transaction
 
 
-class TestFuncService(TestBase):
+def overview_settings(browser):
+    browser.inspect.add('results', '//span[@class="path"]/a')
+
+
+class ServiceFunctionalTestCase(TestBase):
 
     def setUp(self):
-        super(TestFuncService, self).setUp()
-        self.browser = Browser(self.layer._test_wsgi_application)
-        self.browser.inspect.add('results', '//span[@class="path"]/a')
+        super(ServiceFunctionalTestCase, self).setUp()
 
         factory = self.root.manage_addProduct['Silva']
         factory.manage_addPublication('publication', 'Publication')
@@ -23,43 +25,61 @@ class TestFuncService(TestBase):
         self.service.build()
         transaction.commit()
 
+    def test_authentication_required(self):
+        browser = self.layer.get_browser(overview_settings)
+        self.assertEqual(
+            browser.open('/root/service_securityoverview/manage_main'),
+            401)
+
     def test_simple_path_query_results(self):
-        status = self.browser.open(
+        browser = self.layer.get_browser(overview_settings)
+        browser.login('manager', 'manager')
+
+        status = browser.open(
             '/root/service_securityoverview/manage_main',
             query={'form.action.search': 'Search',
                    'form.field.path': '/root'})
         self.assertEquals(200, status)
-        paths = self.browser.inspect.results
-        self.assertEquals(['/root/publication', '/root/publication/test_link'],
-                          paths)
+        self.assertEquals(
+            ['/root/publication', '/root/publication/test_link'],
+            browser.inspect.results)
 
     def test_simple_path_and_role(self):
-        status = self.browser.open(
+        browser = self.layer.get_browser(overview_settings)
+        browser.login('manager', 'manager')
+
+        status = browser.open(
             '/root/service_securityoverview/manage_main',
             query={'form.action.search': 'Search',
                    'form.field.path': '/root/publication',
                    'form.field.role': 'Viewer +'})
         self.assertEquals(200, status)
-        paths = self.browser.inspect.results
-        self.assertEquals(['/root/publication/test_link'],
-                          paths)
+        self.assertEquals(
+            ['/root/publication/test_link'],
+            browser.inspect.results)
 
     def test_simple_path_and_user(self):
-        status = self.browser.open(
+        browser = self.layer.get_browser(overview_settings)
+        browser.login('manager', 'manager')
+
+        status = browser.open(
             '/root/service_securityoverview/manage_main',
             query={'form.action.search': 'Search',
                    'form.field.path': '/root/publication',
                    'form.field.user': 'viewer'})
         self.assertEquals(200, status)
-        paths = self.browser.inspect.results
-        self.assertEquals(['/root/publication/test_link'],
-                          paths)
+        self.assertEquals(
+            ['/root/publication/test_link'],
+            browser.inspect.results)
 
     def test_path_replace_relative_path_in_query(self):
-        status = self.browser.open(
+        browser = self.layer.get_browser(overview_settings)
+        browser.login('manager', 'manager')
+
+        status = browser.open(
             '/root/service_securityoverview/manage_main',
             query={'form.action.search': 'Search',
                    'form.field.path': 'publication'})
         self.assertEquals(200, status)
-        field = self.browser.html.xpath('//input[@name="form.field.path"]')[0]
+        field = browser.html.xpath('//input[@name="form.field.path"]')[0]
         self.assertEquals('/root/publication', field.get('value'))
